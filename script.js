0bd5a4c4eb82ad5fb0698f2e73b9cec8042cfe9d
Todd Wolfson <todd@twolfson.com>
2023-05-28 14:06:06 -0700

    Starting to abstract AngelList parsing

--- script.js
+++ script.js
@@ -4,3 +4,32 @@
 // @include  https://wellfound.com/jobs*
 // @grant    none
 // ==/UserScript==
+
+const $ = document.querySelector
+const $$ = document.querySelectorAll
+
+class AngelListCompanyResult {
+  constructor(el) {
+    this.el = el;
+  }
+
+  getName() {
+
+  }
+}
+
+// Define our common function
+const main = () => {
+  // Resolve our company cards
+  // TODO: Abstract to common cross-site setup
+  const companyEls = $$('data-test="StartupResult"');
+
+  console.log('hi');
+}
+
+// When the page loads
+window.addEventListener('DOMContentLoaded', (evt) => {
+  main();
+  // TODO: Add debouncing
+  new MutationObserver(main).observe($('body'), {childList: true, subtree: true})
+});
